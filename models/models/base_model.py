31c490f335147ff1413029a6cc0f11241374db1e
Alafresh <868@holbertonschool.com>
2019-11-07 17:06:39 -0500

    type JSON my model

--- models/models/base_model.py
+++ models/models/base_model.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from uuid import uuid4
-from json import loads
-from datetime import datetime
+from json import loads, dumps
+from datetime import datetime, date
 
-class BaseModel:
+class BaseModel():
     """Base Class"""
 
-    def __init__(self, id=None):
-        id = uuid4()
-        created_at = datetime.now()
-        update_at = datetime.now()
+    def __init__(self):
+        
+        self.id = str(uuid4())
+        self.update_at = datetime.now()
+        self.created_at = datetime.now()
 
     def __str__(self):
-        print("[{}] ({}) {}".format(self.__name__(), self.id, self.__dict__()))
+        nameClass = self.__class__.__name__
+        return "[{}] ({}) {}".format(nameClass, self.id, self.__dict__)
 
     def save(self):
         self.update_at = datetime.now()
 
     def to_dict(self):
-        return self.__dict__()
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['update_at'] = str(datetime.isoformat(self.update_at))
+        dictionary['created_at'] = str(self.created_at)
+        return dictionary
--- models/test.py
+++ models/test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #!/usr/bin/python3
-from models.base_model import base_model
+from models.base_model import BaseModel
 
 my_model = BaseModel()
 my_model.name = "Holberton"
