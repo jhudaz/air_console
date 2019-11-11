d0f408b1370f2581034a68a14f955acc2d416b7b
jandres <jandres.vr@hotmail.com>
2019-11-11 16:43:22 -0500

    feature: changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
 """ storage model """
-import json
-from os import path
+from json import dump, load
+from datetime import datetime
 
 
 class FileStorage():
     """FileStorage class"""
 
     """private class attributes"""
-    __file_path = ''
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """ """
-        return FileStorage.__objects;
+        return FileStorage.__objects
 
     def new(self, obj):
-        """ """
-        return ''
+        """"""
+        nameClass = obj.__class__.__name__
+        idClass = obj.id
+        key = nameClass+'.'+idClass
+        dataDict = obj.__dict__.copy()
+        created_date = dataDict['created_at']
+        update_date = dataDict['update_at']
+        dataDict['created_at'] = str(datetime.isoformat(created_date))
+        dataDict['update_at'] = str(datetime.isoformat(update_date))
+        FileStorage.__objects.update({key: dataDict})
+        print("\n\ndata {}\n\n".format(dataDict))
 
+       
     def save(self):
         """ serialize the object to a JSON file"""
-        
-        return ''
+        with open(FileStorage.__file_path, 'a', encoding='utf-8') as file:
+            dataJson = FileStorage.__objects
+            dump(dataJson, file)
 
     def reload(self):
         """ """
-        return ''
+        """with open(FileStorage.__file_path, 'r', encoding='utf-8')as file:
+            load(file)"""
